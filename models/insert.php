c82f94477b875676658482bd01c840daf48cd6c1
David Pérez <davidpe1307@gmail.com>
2026-06-05 12:18:42 -0500

    Corrigo insert

--- models/insert.php
+++ models/insert.php
@@ -1,20 +1,19 @@
 <?php
     include "conexion.php";
-    $proID=$_GET["proID"];
-    $proNombre=$_GET["proNombre"];
-    $proDescripcion=$_GET["proDescripcion"];
-    $proPrecio=$_GET["proPrecio"];
-    $proStock=$_GET["proStock"];
-    $proImagen=$_GET["proImagen"];
 
+    $proNombre      = $_POST["proNombre"];
+    $proDescripcion = $_POST["proDescripcion"];
+    $proPrecio      = $_POST["proPrecio"];
+    $proImagen      = $_POST["proImagen"];
 
-    $sqlInsert= "insert into productos values ('$proID','$proNombre','$proDescripcion', '$proPrecio', '$proStock', '$proImagen')";
+    $sqlInsert = "INSERT INTO productos (nombre, descripcion, precio, imagen) 
+                  VALUES ('$proNombre', '$proDescripcion', '$proPrecio', '$proImagen')";
 
-    $respuesta=$conn->query($sqlInsert);
+    $respuesta = $conn->query($sqlInsert);
 
-    if ($respuesta == TRUE){
-        echo ("Se inserto el producto");
+    if ($respuesta == TRUE) {
+        echo "Se insertó el producto correctamente.";
     } else {
-        echo ("No se inserto el producto");
+        echo "Error al insertar: " . $conn->error;
     }
 ?>
